6fdb252efbecf58174b775faf4a15bd10355c3fd
lecbit <artm-marych@rambler.ru>
2023-03-08 22:40:37 +0000

    Update_v1.5

--- scripts/script.js
+++ scripts/script.js
@@ -17,14 +17,22 @@ let one = document.getElementById('one');
 let two = document.getElementById('two');
 let three = document.getElementById('three');
 
-let name1;
-let name2;
-
 let player1_text = document.getElementById('player_name1_text');
 let player_name1 = document.getElementById('player_name1');
 
 let player2_text = document.getElementById('player_name2_text');
 let player_name2 = document.getElementById('player_name2');
+
+let score_settings_data = document.getElementById('score_settings_data');
+let games_settings_data = document.getElementById('games_settings_data');
+let innings_settings_data = document.getElementById('innings_settings_data');
+
+let option = document.createElement('option');
+
+let list = document.getElementById('browsers');
+
+let name1;
+let name2;
 
 let left_counter = 0;
 let right_counter = 0;
@@ -33,6 +41,7 @@ let left_round_count = 0;
 let right_round_count = 0;
 
 let storona_poda = 0;
+let start_poda = 0;
 let left_ball = 1;
 let right_ball = 0;
 
@@ -44,25 +53,38 @@ let nameChangeMode = 0;
 let changeSettingsMode = 0;
 let mode = 0;
 
+let guitarMode = 1;
+let clapsMode = 1;
+
 let gitar = new Audio();
 gitar.src = "./sounds/gitara.mp3";
 
 let claps = new Audio();
 claps.src = './sounds/claps.mp3';
 
-//
+//Game settings
 if (JSON.parse(localStorage.getItem('gameSettings'))) {
-    score_settings = JSON.parse(localStorage.getItem('gameSettings'))['score'];
-    score_text.innerText = score_settings;
+    if (JSON.parse(localStorage.getItem('gameSettings'))['score']) {
+        score_settings = JSON.parse(localStorage.getItem('gameSettings'))['score'];
+        score_text.innerText = score_settings;
+        score_settings_data.value = JSON.parse(localStorage.getItem('gameSettings'))['score'];
+    }
 
-    games_number_settings = JSON.parse(localStorage.getItem('gameSettings'))['games'];
-    games_number_text.innerText = games_number_settings;
+    if (JSON.parse(localStorage.getItem('gameSettings'))['games']) {
+        games_number_settings = JSON.parse(localStorage.getItem('gameSettings'))['games'];
+        games_number_text.innerText = games_number_settings;
+        games_settings_data.value = JSON.parse(localStorage.getItem('gameSettings'))['games'];
 
-    innings_number_settings = JSON.parse(localStorage.getItem('gameSettings'))['innings'];
-    innings_number_text.innerText = innings_number_settings;
+    }
+
+    if (JSON.parse(localStorage.getItem('gameSettings'))['innings']) {
+        innings_number_settings = JSON.parse(localStorage.getItem('gameSettings'))['innings'];
+        innings_number_text.innerText = innings_number_settings;
+        innings_settings_data.value = JSON.parse(localStorage.getItem('gameSettings'))['innings'];
+    }
 }
 
-//
+//Game saver
 if (JSON.parse(localStorage.getItem('gameSave'))) {
     left_counter = JSON.parse(localStorage.getItem('gameSave'))['left_counter'];
     right_counter = JSON.parse(localStorage.getItem('gameSave'))['right_counter'];
@@ -95,17 +117,42 @@ if (JSON.parse(localStorage.getItem('gameSave'))) {
         }
     }
 
-    mode = 0;
+    mode = JSON.parse(localStorage.getItem('gameSave'))['mode'];
+}
+
+//Sounds OFF
+if (localStorage.getItem('guitar')) {
+    guitarMode = 0;
+    document.getElementById('guitar_Input').removeAttribute('checked')
+}
+if (localStorage.getItem('claps')) {
+    clapsMode = 0;
+    document.getElementById('claps_Input').removeAttribute('checked')
+}
+
+//Names
+if (JSON.parse(localStorage.getItem('savedNames'))) {
+    let namesLoad = JSON.parse(localStorage.getItem('savedNames'));
+
+
+    namesLoad.forEach(function (item) {
+        var option = document.createElement('option');
+        option.value = item;
+        list.appendChild(option);
+    });
 }
 
 //
-if (JSON.parse(localStorage.getItem('savedNames'))) {
-    name1 = JSON.parse(localStorage.getItem('savedNames'))['name1'];
-    player1_text.innerText = name1;
+if (JSON.parse(localStorage.getItem('actualNames'))) {
+    if (JSON.parse(localStorage.getItem('actualNames'))['name1']) {
+        name1 = JSON.parse(localStorage.getItem('actualNames'))['name1'];
+        player1_text.innerText = name1;
+    }
 
-    name2 = JSON.parse(localStorage.getItem('savedNames'))['name2'];
-    player2_text.innerText = name2;
-
+    if (JSON.parse(localStorage.getItem('actualNames'))['name2']) {
+        name2 = JSON.parse(localStorage.getItem('actualNames'))['name2'];
+        player2_text.innerText = name2;
+    }
 }
 
 
@@ -147,7 +194,26 @@ function roundRestart() {
     text_count_left.innerHTML = left_counter;
     right_counter = 0;
     text_count_right.innerHTML = right_counter;
-    smenaStorony();
+    podachaStartRaund();
+}
+
+function podachaStartRaund()
+{
+    if (start_poda == 0) {
+        start_poda = 1;
+        left_ball = 0;
+        right_ball = 1;
+        poda1.innerHTML = '';
+        poda2.innerHTML = '<div class="mbox col-2 rounded-circle"></div>';
+    }
+    else {
+        start_poda = 0;
+        left_ball = 1;
+        right_ball = 0;
+        poda2.innerHTML = '';
+        poda1.innerHTML = '<div class="mbox col-2 rounded-circle"></div>';
+    }
+    gameSaver();
 }
 
 function PodachaCheck() {
@@ -166,7 +232,9 @@ function leftBallAdd() {
         poda1.innerHTML = poda1.innerHTML + '<div class="mbox col-2 rounded-circle"></div>';
     }
     else {
-        gitar.play();
+        if (guitarMode == 1) {
+            gitar.play();
+        }
         smenaStorony();
     }
 }
@@ -177,7 +245,9 @@ function rightBallAdd() {
         poda2.innerHTML = poda2.innerHTML + '<div class="mbox col-2 rounded-circle"></div>';
     }
     else {
-        gitar.play();
+        if (guitarMode == 1) {
+            gitar.play();
+        }
         smenaStorony();
     }
 }
@@ -198,8 +268,10 @@ function pointsChecker() {
 }
 
 function leftRoundChecker() {
-    if (left_round_count >= games_number_settings) {
-        claps.play();
+    if (left_round_count == games_number_settings) {
+        if (clapsMode == 1) {
+            claps.play();
+        }
         localStorage.removeItem('gameSave');
         alert('Left winner!');
     }
@@ -209,8 +281,10 @@ function leftRoundChecker() {
 }
 
 function rightRoundChecker() {
-    if (right_round_count >= games_number_settings) {
-        claps.play();
+    if (right_round_count == games_number_settings) {
+        if (clapsMode == 1) {
+            claps.play();
+        }
         localStorage.removeItem('gameSave');
         alert('Right winner!');
     }
@@ -271,6 +345,7 @@ function gameSaver() {
         storona_poda: storona_poda,
         left_ball: left_ball,
         right_ball: right_ball,
+        mode: mode,
     }
     localStorage.setItem('gameSave', JSON.stringify(GAME_SAVE));
 }
@@ -281,10 +356,14 @@ function playMode() {
     if (storona_poda == 0) {
         poda2.innerHTML = '<div class="mbox col-2 rounded-circle"></div>';
         storona_poda = 1;
+        left_ball = 0;
+        right_ball = 1;
     }
     else {
         poda1.innerHTML = '<div class="mbox col-2 rounded-circle"></div>';
         storona_poda = 0;
+        left_ball = 1;
+        right_ball = 0;
     }
     if (left_counter - right_counter == 2) {
         left_round_count += 1;
@@ -298,39 +377,81 @@ function playMode() {
         mode = 0;
         rightRoundChecker();
     }
+    console.log(3443);
+    gameSaver();
+}
+
+let parameters_button = document.getElementById('parameters_button');
+let parameters_yes = document.getElementById('parameters_yes');
+let parameters_no = document.getElementById('parameters_no');
+
+function cancelSettings() {
+    parameters_button.style.display = 'block';
+    parameters_yes.style.display = 'none';
+    parameters_no.style.display = 'none';
+
+    score_text.style.display = 'block';
+    score_settings_data.style.display = 'none';
+    games_number_text.style.display = 'block';
+    games_settings_data.style.display = 'none';
+    innings_number_text.style.display = 'block';
+    innings_settings_data.style.display = 'none';
+
+    score_settings_data.value = '';
+
+    games_settings_data.value = '';
+
+    innings_settings_data.value = '';
+
+    changeSettingsMode = 0;
 }
 
 function changeSettings() {
     if (changeSettingsMode == 0) {
-        one.innerHTML = `<input id="score_settings" type="number" size="3" placeholder="21" value="${score_settings}" />`;
-        two.innerHTML = `<input id="games_number_settings" type="number" size="3" placeholder="3" value="${games_number_settings}" />`;
-        three.innerHTML = `<input id="innings_number_settings" type="number" size="3" placeholder="5" value="${innings_number_settings}" />`;
+        parameters_button.style.display = 'none';
+        parameters_yes.style.display = 'block';
+        parameters_no.style.display = 'block';
+    }
+    else {
+        parameters_button.style.display = 'block';
+        parameters_yes.style.display = 'none';
+        parameters_no.style.display = 'none';
+    }
+
+    if (changeSettingsMode == 0) {
+        score_text.style.display = 'none';
+        score_settings_data.style.display = 'block';
+        games_number_text.style.display = 'none';
+        games_settings_data.style.display = 'block';
+        innings_number_text.style.display = 'none';
+        innings_settings_data.style.display = 'block';
+
+        if (score_settings_data.value) {
+            score_settings_data.value = score_settings;
+        }
+
+        if (games_settings_data.value) {
+            games_settings_data.value = games_number_settings;
+        }
+
+        if (innings_settings_data.value) {
+            innings_settings_data.value = innings_number_settings;
+        }
+
         changeSettingsMode = 1;
     }
     else {
-        restartGame();
-        if (Number(document.getElementById('score_settings').value)) {
-            score_settings = document.getElementById('score_settings').value;
-        }
-        else {
-            score_settings = 21;
-        }
-        if (Number(document.getElementById('games_number_settings').value)) {
-            games_number_settings = document.getElementById('games_number_settings').value;
-        }
-        else {
-            games_number_settings = 3;
-        }
-        if (Number(document.getElementById('innings_number_settings').value)) {
-            innings_number_settings = document.getElementById('innings_number_settings').value;
-        }
-        else {
-            innings_number_settings = 5;
-        }
+        score_text.style.display = 'block';
+        score_settings_data.style.display = 'none';
+        games_number_text.style.display = 'block';
+        games_settings_data.style.display = 'none';
+        innings_number_text.style.display = 'block';
+        innings_settings_data.style.display = 'none';
 
-        one.innerHTML = score_settings;
-        two.innerHTML = games_number_settings;
-        three.innerHTML = innings_number_settings;
+
+        score_settings = score_settings_data.value;
+        games_number_settings = games_settings_data.value;
+        innings_number_settings = innings_settings_data.value;
 
         changeSettingsMode = 0;
 
@@ -341,6 +462,32 @@ function changeSettings() {
             innings: innings_number_settings,
         }
         localStorage.setItem('gameSettings', JSON.stringify(GAME_SETTINGS));
+        //Задать дефолт значения, если данные не введены. Порядок выполнения строк важен!!!
+        if (score_settings) {
+            score_text.innerText = score_settings;
+        }
+        else {
+            score_settings = 21;
+            score_text.innerText = 21;
+        }
+
+        if (games_number_settings) {
+            games_number_text.innerText = games_number_settings;
+        }
+        else {
+            games_number_settings = 3;
+            games_number_text.innerText = 3;
+        }
+
+        if (innings_number_settings) {
+            innings_number_text.innerText = innings_number_settings;
+        }
+        else {
+            innings_number_settings = 2;
+            innings_number_text.innerText = 2;
+        }
+
+        restartGame();
     }
 }
 
@@ -373,27 +520,99 @@ function namesChanger() {
 
 
 
-        if (name1 == 0) {
-            name1 = 'Name1';
-            player1_text.innerText = 'Name1';
-        }
-        else {
+        if (name1 != 0) {
             player1_text.innerText = name1;
         }
+        else {
+            player1_text.innerText = 'Name1';
+        }
 
-        if (name2 == 0) {
-            name2 = 'Name2';
-            player2_text.innerText = 'Name2';
+        if (name2 != 0) {
+            player2_text.innerText = name2;
         }
         else {
-            player2_text.innerText = name2;
+            player2_text.innerText = 'Name2';
         }
 
         //
-        const SAVED_NAMES = {
+        const ACTUAL_NAMES = {
             name1: name1,
             name2: name2,
         }
-        localStorage.setItem('savedNames', JSON.stringify(SAVED_NAMES));
+        localStorage.setItem('actualNames', JSON.stringify(ACTUAL_NAMES));
+
+        namesSaver();
+    }
+}
+
+function namesSaver() {
+    let names;
+    if (JSON.parse(localStorage.getItem('savedNames'))) {
+        names = JSON.parse(localStorage.getItem('savedNames'));
+
+        if (name1 && !names.includes(name1)) { //Если имя не пустое и такого в массиве нет.
+            names.push(name1);
+        }
+
+        if (name2 && !names.includes(name2)) { //Если имя не пустое и такого в массиве нет.
+            names.push(name2);
+        }
+
+        if (Object.keys(names).length > 5) {
+            list.innerHTML = '';
+            let number_elements = Object.keys(names).length;
+            names = names.slice(number_elements - 5, number_elements)
+        }
+
+        localStorage.setItem('savedNames', JSON.stringify(names));
+    }
+    else {
+        let names = [];
+
+        if (name1) {
+            names.push(name1);
+        }
+
+        if (name2 && !names.includes(name2)) {
+            names.push(name2);
+        }
+
+        localStorage.setItem('savedNames', JSON.stringify(names));
+
+        array = JSON.parse(localStorage.getItem("savedNames"));
+    }
+
+    // 
+    let namesLoad = JSON.parse(localStorage.getItem('savedNames'));
+
+    list.innerHTML = '';
+
+    names.forEach(function (item) {
+        var option = document.createElement('option');
+        option.value = item;
+        list.appendChild(option);
+    });
+}
+
+function clapsSwitch() {
+    if (localStorage.getItem('claps')) {
+        localStorage.removeItem('claps');
+        clapsMode = 1;
+    }
+    else {
+        localStorage.setItem('claps', '1');
+        clapsMode = 0;
+    }
+
+}
+
+function guitarSwitch() {
+    if (localStorage.getItem('guitar')) {
+        localStorage.removeItem('guitar');
+        guitarMode = 1;
+    }
+    else {
+        localStorage.setItem('guitar', '1');
+        guitarMode = 0;
     }
 }
